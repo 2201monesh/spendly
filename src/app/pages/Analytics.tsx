9024bfcbf1c3b480b6cf3623f2b9eca5550f330e
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-06-09 23:00:25 +0530

    Changes

--- src/app/pages/Analytics.tsx
+++ src/app/pages/Analytics.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-
 function Analytics() {
-  return <div>Analytics Page</div>;
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <p className="px-7">Analytics</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Analytics;
