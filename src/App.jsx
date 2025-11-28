46b8e14efb6cdab0665bc2ef9855cce6608ebfc0
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-11-28 23:17:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <p class="text-8xl font-bold underline">hello</p>;
+}
+
+export default App;
