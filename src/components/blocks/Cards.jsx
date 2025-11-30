c762728ec31632e50b0c49faee24dae884dd1837
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-11-30 23:28:41 +0530

    Added logout functionality

--- src/components/blocks/Cards.jsx
+++ src/components/blocks/Cards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Cards({ heading, logo }) {
   return (
-    <div className="w-96 h-32 rounded-sm p-3 border">
+    <div className="w-96 h-32 rounded-sm p-3 border dark:bg-[#18181b]">
       <h1 className="font-geist text-md mt-1">{heading}</h1>
       <div className="flex justify-between items-center mt-2">
         <span className="text-4xl">₹ 90,000</span>
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import ToggleSwitch from "@/components/ui/ToggleSwitch";
+import { supabase } from "@/lib/supabaseClient";
 import React from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 function MainLayout() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const pageTitles = {
     "/dashboard": "Dashboard",
@@ -14,13 +17,25 @@ function MainLayout() {
 
   const title = pageTitles[location.pathname] || "Page";
 
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    navigate("/");
+  };
+
   return (
     <div className="w-full h-screen border">
       <div className="w-full border-b p-3 h-14 flex items-center font-geist justify-between">
         <span>{title}</span>
-        <span>
+        <div className="flex items-center gap-4">
           <ToggleSwitch />
-        </span>
+          <Button
+            variant="outline"
+            className="cursor-pointer"
+            onClick={handleLogout}
+          >
+            Logout
+          </Button>
+        </div>
       </div>
       <div className="flex-1 overflow-auto p-4">
         <Outlet />
