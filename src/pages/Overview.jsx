fc5e47ba5c7163bdc918dd6f6621c1dcede3e5c8
Monesh Goyal <goyalmonesh2201@gmail.com>
2025-12-01 20:12:27 +0530

    Added chart in overview section

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,16 +1,40 @@
 import Cards from "@/components/blocks/Cards";
+import ChartAreaInteractive from "@/components/charts/ChartAreaInteractive";
 import React from "react";
 import { BiMoneyWithdraw } from "react-icons/bi";
-import { PiChartLineUp } from "react-icons/pi";
-import { PiChartLineDown } from "react-icons/pi";
+import { PiChartLineUp, PiChartLineDown } from "react-icons/pi";
+
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Tooltip,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "Jan", income: 4000, expense: 2400 },
+  { name: "Feb", income: 3000, expense: 1398 },
+  { name: "Mar", income: 5000, expense: 2800 },
+  { name: "Apr", income: 4780, expense: 1908 },
+  { name: "May", income: 5890, expense: 4800 },
+  { name: "Jun", income: 4390, expense: 3800 },
+];
 
 function Overview() {
   return (
-    <div className="flex justify-center ">
-      <div className="flex gap-4">
+    <div className="flex flex-col items-center w-full">
+      {/* Cards Section */}
+      <div className="flex gap-4 mb-8">
         <Cards heading="Total Balance" logo={<BiMoneyWithdraw size={30} />} />
         <Cards heading="Total Expense" logo={<PiChartLineDown size={30} />} />
         <Cards heading="Total Income" logo={<PiChartLineUp size={30} />} />
+      </div>
+
+      <div className="w-full">
+        <ChartAreaInteractive />
       </div>
     </div>
   );
